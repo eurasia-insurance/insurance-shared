8d1f7baff0a3dbb1e74a566473f0461091bdea63
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-21 09:38:20 +0600

    push destinations removed

--- src/main/java/tech/lapsa/insurance/shared/jms/InsuranceDestinations.java
+++ src/main/java/tech/lapsa/insurance/shared/jms/InsuranceDestinations.java
@@ -9,9 +9,5 @@ public final class InsuranceDestinations {
     public static final String NOTIFIER_NEW_CASCO_USER_EMAIL = "insurance/jms/events/notifier/cascoNewUserEmail";
     public static final String NOTIFIER_NEW_POLICY_COMPANY_EMAIL = "insurance/jms/events/notifier/policyNewCompanyEmail";
     public static final String NOTIFIER_NEW_POLICY_USER_EMAIL = "insurance/jms/events/notifier/policyNewUserEmail";
-    public static final String NOTIFIER_NEW_INSURANCE_COMPANY_PUSH = "insurance/jms/events/notifier/newInsuranceCompanyPush";
-    public static final String NOTIFIER_NEW_CALLBACK_COMPANY_PUSH = "insurance/jms/events/notifier/newCallbackCompanyPush";
     public static final String NOTIFIER_REQUEST_PAID_COMPANY_EMAIL = "insurance/jms/events/notifier/requestPaidCompanyEmail";
-
-    public static final String NOTIFIER_PUSH_JOBS = "insurance/jms/events/notifier/pushJobs";
 }
